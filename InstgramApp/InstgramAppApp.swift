20e3e9bd55d5bc30889b01cda5d0539233047804
CoderEslam <eslamghazy600@gmail.com>
2023-07-08 11:16:37 +0200

    Initial Commit

---
+++ InstgramApp/InstgramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstgramAppApp.swift
+//  InstgramApp
+//
+//  Created by Eslam Ghazy on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct InstgramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
